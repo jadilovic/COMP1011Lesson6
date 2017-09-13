d4395835f82e1265c01bb03164edc61a4cf8ebff
Win7 <Win7@Win7-PC>
2017-09-13 19:47:59 +0200

    Vehicle class

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,4 +1,26 @@
 
 public abstract class Vehicle {
 
+	// INSTANT VERIABLES (FIELDS) +++++++++++++++++++++
+	private int _tireCount;
+	/**
+	 * @return the _tireCount
+	 */
+	public int get_tireCount() {
+		return _tireCount;
+	}
+
+	/**
+	 * @param _tireCount the _tireCount to set
+	 */
+	public void set_tireCount(int _tireCount) {
+		this._tireCount = _tireCount;
+	}
+	
+	
+	// CONSTRUCTOR ++++++++++++++++++++++++++++++++++++
+	public void Vehicle(){
+		
+	}
+
 }
