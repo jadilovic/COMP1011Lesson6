9760ac137b5f3e583052fec86e54c01809fdab78
Win7 <Win7@Win7-PC>
2017-09-13 19:47:39 +0200

    Created Abstract Vehicle Class

---
+++ src/Program.java
@@ -0,0 +1,11 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Object myObject = new Object();
+		String myString = myObject.toString();
+		System.out.println(myString);
+	}
+
+}
